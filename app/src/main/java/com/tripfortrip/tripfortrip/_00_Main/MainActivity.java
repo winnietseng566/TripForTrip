4992ce08afe0281585b636ad25860bc52a5f8949
winnie <winnietseng566@gmail.com>
2017-02-16 17:04:14 +0800

    first commit(winnie)

---
+++ app/src/main/java/com/tripfortrip/tripfortrip/_00_Main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tripfortrip.tripfortrip._00_Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tripfortrip.tripfortrip.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
