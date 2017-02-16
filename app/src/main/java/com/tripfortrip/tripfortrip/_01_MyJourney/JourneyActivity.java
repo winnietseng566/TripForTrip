b89b2f99eab2ce1707476c26bae4c3e6278a76e9
winnie <winnietseng566@gmail.com>
2017-02-16 21:50:31 +0800

    second commit(winnie)

---
+++ app/src/main/java/com/tripfortrip/tripfortrip/_01_MyJourney/JourneyActivity.java
@@ -0,0 +1,15 @@
+package com.tripfortrip.tripfortrip._01_MyJourney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tripfortrip.tripfortrip.R;
+
+public class JourneyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journey);
+    }
+}
