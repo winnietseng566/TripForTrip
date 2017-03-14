a91d7c543a685b86e71f9ed3b483e42719ba00e6
Aso <cute17852337569@gmail.com>
2017-03-14 11:34:14 +0800

    輸入座標能查詢地址

--- app/src/main/java/com/tripfortrip/tripfortrip/_00_Main/MapFragment.java
+++ app/src/main/java/com/tripfortrip/tripfortrip/_00_Main/MapFragment.java
@@ -1,5 +1,8 @@
 package com.tripfortrip.tripfortrip._00_Main;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,12 +11,42 @@ import android.view.ViewGroup;
 
 import com.tripfortrip.tripfortrip.R;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements LocationListener{
+
+
+    static final int MIN_TIME = 5000;  // 位置更新條件：5000毫秒
+    static final float MIN_DIST = 0;   // 位置更新條件：5公尺
+    LocationManager mgr;               // 定位管理員
+
+    boolean isGPSEnabled;        // GPS定位是否可用
+    boolean isNetworkEnabled;    // 網路定位是否可用
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.map_fragment, container, false);
         return view;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
--- app/src/main/java/com/tripfortrip/tripfortrip/_02_SpotSearch/SearchActivity.java
+++ app/src/main/java/com/tripfortrip/tripfortrip/_02_SpotSearch/SearchActivity.java
@@ -1,8 +1,9 @@
 package com.tripfortrip.tripfortrip._02_SpotSearch;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,18 +12,16 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.tripfortrip.tripfortrip.Manifest;
 import com.tripfortrip.tripfortrip.R;
 
+import java.util.List;
+import java.util.Locale;
+
 public class SearchActivity extends AppCompatActivity implements LocationListener {
-
-    LocationManager locmgr = (LocationManager)
-            getSystemService(Context.LOCATION_SERVICE);
-
-
 
     static final int MIN_TIME = 5000;
     static final float MIN_DIST = 0;
@@ -32,6 +31,10 @@ public class SearchActivity extends AppCompatActivity implements LocationListene
 
     boolean isGPSEnabled;
     boolean isNetworkEnabled;
+
+    Location myLocation;        // 儲存最近的定位資料
+    Geocoder geocoder;          // 用來查詢地址的 Geocoder 物件
+    EditText edtLat, edtLon;    // 經緯度輸入欄位
 
 
 
@@ -48,6 +51,11 @@ public class SearchActivity extends AppCompatActivity implements LocationListene
         mgr = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         checkPermission(); // 檢查若尚未授權，則向使用者要求定位權限
+
+
+        edtLat = (EditText) findViewById(R.id.edtLan);
+        edtLon = (EditText) findViewById(R.id.edtLon);
+        geocoder = new Geocoder(this, Locale.getDefault());  // 建立 Geocoder 物件
     }
 
     @Override
@@ -79,7 +87,7 @@ public class SearchActivity extends AppCompatActivity implements LocationListene
 
     private void enableLocationUpdates(boolean isTurnOn) {
         if (ActivityCompat.checkSelfPermission(this,
-                Manifest.ACCESS_FINE_LOCATION)
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED)
         {
             if (isTurnOn){
@@ -114,11 +122,11 @@ public class SearchActivity extends AppCompatActivity implements LocationListene
     // 檢查若尚未授權，則向使用者要求定位權限
     private void checkPermission() {
         if (ActivityCompat.checkSelfPermission(this,
-                Manifest.ACCESS_FINE_LOCATION)
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED)
         {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},200);
+                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},200);
         }
     }
 
@@ -128,9 +136,53 @@ public class SearchActivity extends AppCompatActivity implements LocationListene
         startActivity(it);
     }
 
+    public void getLocation(View v){
+        if (myLocation != null){
+            edtLat.setText(Double.toString(myLocation.getLatitude()));  // 將經度轉成字串
+            edtLon.setText(Double.toString(myLocation.getLongitude())); // 將緯度轉成字串
+        } else {
+            txvLoc.setText("無法取得定位資料！");
+        }
+    }
+
+    public void onQuery(View view){  // 用經緯度查地址"按鈕 onClick 事件"
+        String strLat = edtLat.getText().toString(); // 取輸入的緯度字串
+        String strLon = edtLon.getText().toString(); // 取輸入的經度字串
+        if (strLat.length() == 0 || strLon.length() == 0){  //當字串為空白時 結束處理
+            return;
+        }
+        txvLoc.setText("讀取中...");
+        double latitude = Double.parseDouble(strLat); // 取得緯度值
+        double longitute = Double.parseDouble(strLon);// 取得經度值
+
+        String strAdrr = "";  // 用來建立所要顯示的訊息字串（地址字串）
+        try {
+            List<Address> listAddr = geocoder.getFromLocation(latitude, longitute, 1);
+                                                        // 用經緯度查地址
+                                                        // 只需要傳回1筆地址資料
+
+            if (listAddr == null || listAddr.size() == 0){  // 檢查是否有取得地址
+                strAdrr += "無法取得地址資料！";
+            } else {                                        // 取 List 中的第一筆
+                Address addr = listAddr.get(0);             // （也是唯一的一筆）
+                for (int i = 0 ; i <= addr.getMaxAddressLineIndex() ; i++){
+                    strAdrr += addr.getAddressLine(i) + "\n";
+                    if (addr.getCountryCode() != null){
+                        strAdrr += addr.getCountryCode();  // 查國碼
+                    }
+                }
+            }
+        } catch (Exception ex){
+            strAdrr += "取得地址發生錯誤：" + ex.toString();
+        }
+        txvLoc.setText(strAdrr);
+    }
+
 
     @Override
     public void onLocationChanged(Location location) {     // 位置變更事件
+
+        myLocation = location;  // 儲存定位資料
 
         String str = "定位提供者：" + location.getProvider();
         str += String.format("\n緯度:%.5f\n經度:%.5f\n高度:%.2f公尺",
